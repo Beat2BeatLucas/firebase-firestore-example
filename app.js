a24f4e0d264190b4a1e65e4646392a38482b2061
Lucas Acuña <lucasnahuelnet@gmail.com>
2020-03-19 12:29:52 -0300

    Updating data

--- app.js
+++ app.js
@@ -58,3 +58,8 @@ db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
         }
     })
 })
+
+//Updating Data
+db.collection('cafes').doc('YoVo5OzFYiBwvZ9B1Z3M').update({ name: 'Wario´s Wonderland'});
+//Tener cuidado con .set() en lugar de .update() ya que el primero sobreescribe
+//el documento.
