17ea5e4df29562cbdae973d32c49bb3b1d6c2d4c
Lucas Acuña <lucasnahuelnet@gmail.com>
2020-03-18 23:12:24 -0300

    Deleting Data

--- app.js
+++ app.js
@@ -7,15 +7,25 @@ function renderCafe(doc){
     let li = document.createElement('li');
     let name = document.createElement('span');
     let city = document.createElement('span');
+    let cross = document.createElement('div');
 
     li.setAttribute('date-id', doc.id);
     name.textContent = doc.data().name;
     city.textContent = doc.data().city;
+    cross.textContent = 'x';
 
     li.appendChild(name);
     li.appendChild(city);
+    li.appendChild(cross);
 
     cafeList.appendChild(li);
+
+    // deleting data
+    cross.addEventListener('click', (e) => {
+        e.stopPropagation();
+        let id = doc.id;
+        db.collection('cafes').doc(id).delete();
+    });
 }
 
 // getting data
